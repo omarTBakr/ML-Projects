f7980e6860829249bfa1cebe32b2b261993fd53b
omarTBakr <bakro0298@gmail.com>
2025-07-07 16:30:16 +0300

    all files'

--- Credit Card Fraud Detection/main.py
+++ Credit Card Fraud Detection/main.py
@@ -1,8 +1,3 @@
-from src.utils import load_model, visualize_all, load_split_test_splitted
-
-if __name__ == "__main__":
-    model = load_model("HardVoting")
-    x_test, y_test =load_split_test_splitted()
-
-
-    visualize_all(model, x_test, y_test)
+version https://git-lfs.github.com/spec/v1
+oid sha256:9e035741ff287b7363fbe94dc76311d900c30919ecf11f98756cbefec1c00f0e
+size 229
